f4dc294d71e5c1c6ef92172f9525cd2742097fdd
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-10-12 01:30:12 +0500

    Scrollbar added

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -96,19 +96,21 @@ const Sidebar = () => {
   ];
 
   return (
-    <div className="flex">
-      <div
-        className={` ${
-          open ? "w-60" : "w-20 "
-        } top-0 left-0 bg-blue-700 h-full p-5  pt-8 relative duration-300`}
-      >
+    <div className="flex relative">
         
-        <div className={`absolute bg-white cursor-pointer -right-3 top-9 w-max border-dark-purple
+        <div className={`absolute bg-white cursor-pointer -right-3 top-9 z-10 w-max border-dark-purple
            border-2 rounded-full  ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}>
             <ChevronLeft/>
         </div>
 
-        <div className="flex gap-x-4 items-center">
+        <div
+            className={` ${
+            open ? "w-60" : "w-20 "
+            } top-0 left-0 bg-blue-700 overflow-hidden py-6 px-4 relative duration-300
+            flex flex-col h-screen
+            `}
+        >
+        <div className="flex-none pb-6 gap-x-4 items-center">
           <h1
             className={`text-white origin-left font-medium text-xl duration-300 cursor-pointer ${
               !open && "scale-0"
@@ -124,13 +126,13 @@ const Sidebar = () => {
             T
           </h1>
         </div>
-        <ul className="pt-6">
-          {menus.map((item, index) => (
+        
+        <ul className="flex-1  transition-all duration-300 overflow-hidden hover:overflow-y-scroll primary-scroll">
+            {menus.map((item, index) => (
                 <Link to={item.link}>
                     <li
                         key={index}
-                        className={`flex rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 hover:bg-blue-600 hover:text-white
-                        ${item.gap ? "mt-9" : "mt-2"} ${
+                        className={`flex rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 hover:bg-blue-600 hover:text-white ${
                             index === 0 && "bg-light-white"
                         } `}
                         >
@@ -140,8 +142,15 @@ const Sidebar = () => {
                             </span>
                     </li>
                 </Link>
-          ))}
+            ))}
         </ul>
+
+        <div className="app-sidebar-footer flex-none flex-column-auto pt-8">
+            <button className="w-full inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-3 bg-slate-900 text-white hover:bg-slate-700">
+            Download
+            </button>
+        </div>
+
       </div>
     </div>
   );
