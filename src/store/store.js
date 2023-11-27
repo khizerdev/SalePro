4d35ebb1c69e91db4c80aa8b40384b65b6e03a2f
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-11-27 12:15:28 +0500

    Redux logger added

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
+import logger from "redux-logger";
 import thunk from "redux-thunk";
 import storage from "redux-persist/lib/storage";
 
@@ -12,7 +13,13 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(persistedReducer, applyMiddleware(thunk));
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === `development`) {
+  middlewares.push(logger);
+}
+
+const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 
 persistStore(store);
 
