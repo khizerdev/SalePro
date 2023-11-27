ce6e27c3e9eb2a1855a75c213cd3b2451640d595
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-11-28 00:15:02 +0500

    Error handling in thunk added

--- src/store/actions/user-actions.js
+++ src/store/actions/user-actions.js
@@ -2,10 +2,17 @@ import axios from "axios";
 
 export const SET_USERS = () => {
   return async (dispatch) => {
-    const result = await axios.get("https://fakestoreapi.com/users");
-    dispatch({
-      type: "SET_USERS",
-      payload: result.data,
-    });
+    try {
+      const result = await axios.get("https://fakestoreapi.com/users");
+      dispatch({
+        type: "SET_USERS",
+        payload: result.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "SET_USERS",
+        payload: [],
+      });
+    }
   };
 };
